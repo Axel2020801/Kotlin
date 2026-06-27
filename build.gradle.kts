abd9575f483aa3ba1238b845a57a476760425249
Axel Bloise <axelarielbloise@gmail.com>
2026-06-27 20:15:24 -0300

    feat: Mover codigo de main a branch relacionada a la API

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+}
+
+group = "com.ecommerce"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.json:json:20240303")
+}
